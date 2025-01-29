f18016ea438433b8817b1d0462a04bc924038113
joab <pbnjoab@gmail.com>
2025-01-29 18:13:58 -0500

    color extracted from image

--- main.py
+++ main.py
@@ -4,13 +4,15 @@ import random
 import colorgram
 
 
-color_list = []
-colors = colorgram.extract("hirst.jpg", 20)
-for color in colors:
-    color_tuple = (color.rgb.r, color.rgb.g,color.rgb.b)
-    color_list.append(color_tuple)
+# color_list = []
+# colors = colorgram.extract("hirst.jpg", 30)
+# for color in colors:
+#     color_tuple = (color.rgb.r, color.rgb.g,color.rgb.b)
+#     color_list.append(color_tuple)
 
-print(color_list)
+color_list = [(252, 138, 52), (49, 171, 90), (120, 185, 140), (246, 252, 248), (249, 75, 110), (250, 231, 236), (247, 223, 64), (250, 77, 50), (120, 120, 67), (226, 212, 16), (19, 37, 103), (3, 111, 166), (156, 76, 130), (52, 148, 24), (254, 167, 160), (2, 59, 126), (247, 250, 253), (199, 192, 28), (223, 122, 146), (130, 166, 194), (245, 159, 176), (161, 211, 175), (51, 178, 183), (110, 121, 157), (176, 189, 216), (152, 208, 218)]
+# color_list was extracted from line 7 - 11
+
 
 screen = Screen()
 screen.exitonclick()
